d69a885e19b19dcbdf963213d92280aa6cbf8cfc
unknown <prepaddoo@gmail.com>
2024-12-19 17:59:42 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Nova Gorica Review - Read Reviews of Nova Gorica Businesses",
   description: "Read and write reviews of Nova Gorica businesses",
+  keywords: [
+    'Nova Gorica Review',
+    'Nova Gorica Attractions',
+    'Nova Gorica Best Places to Visit',
+    'Nova Gorica Businesses',
+    'Living in Nova Gorica',
+    'Things to do in Nova Gorica',
+     ],
 }
 
 export default function RootLayout({
